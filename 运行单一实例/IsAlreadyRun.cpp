98e27ee99a5a6b2bafeab803b07f70961e350f42
reuodut <reuodut@163.com>
2019-10-24 15:58:06 +0800

    add IsAlreadyRun

---
+++ 运行单一实例/IsAlreadyRun.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+BOOL IsAleadyRun()
+{
+	HANDLE hMutex = NULL;
+	hMutex = CreateMutex(NULL, FALSE, L"TEST");
+	if (hMutex) 
+	{ 
+		if (ERROR_ALREADY_EXISTS == GetLastError())
+		{
+			return TRUE;
+		}
+	}
+	return FALSE;
+	//不要使用CloseHandle函数来关闭互斥对象的句柄, 关闭后互斥对象会被释放
+}
+
+int main ()
+{
+	BOOL ret = IsAleadyRun();
+	
+	if (ret)
+	{
+		cout << "TEST is already run !" << endl;
+	}
+	else
+	{
+		cout << "First run of TEST program !" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
